7896211f6001602c76ed3a0ba1176aee3b737a0b
Rodrigo Hernández <54005507+Rodrigo-HH@users.noreply.github.com>
2021-09-08 13:31:00 -0500

    Create Factorial.dart

---
+++ Factorial.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+int a=1;
+int b=1;
+
+int Factorial(int n){
+int c;
+for (c = 1; c <= n; c++){
+b=a*c;
+a=b;
+}
+return b;
+}
+
+void main(){
+print('Inserte un valor para calcular el factorial');
+int n = int.parse(stdin.readLineSync());
+if (n>=0){
+print(Factorial(n));
+}
+else {
+print('Valor invalido');
+}
+}
