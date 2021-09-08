aa86d91afa066d4aac0872a765c82aa36e338539
Rodrigo Hernández <54005507+Rodrigo-HH@users.noreply.github.com>
2021-09-08 13:38:48 -0500

    Add files via upload

--- Factorial.dart
+++ Factorial.dart
@@ -4,21 +4,21 @@ int a=1;
 int b=1;
 
 int Factorial(int n){
-  int c;
-  for (c = 1; c <= n; c++){
-  b=a*c;
-  a=b;
-  }
-  return b;
+int c;
+for (c = 1; c <= n; c++){
+b=a*c;
+a=b;
+}
+return b;
 }
 
 void main(){
-  print('Inserte un valor para calcular el factorial');
-  int n = int.parse(stdin.readLineSync());
-  if (n>=0){
-  print(Factorial(n));
-  }
-  else {
-  print('Valor invalido');
-  }
+print('Inserte un valor para calcular el factorial');
+int? n = int.parse(stdin.readLineSync()!);
+if (n>=0){
+print(Factorial(n));
+}
+else {
+print('Valor invalido');
+}
 }
